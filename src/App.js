b1e91a9763bdd78a4109ca1d618e7e4bf2cd2dda
Alejandro Medina <5almero5@gmail.com>
2021-05-12 18:24:32 -0500

    Add automation of  prettier

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
+import React from 'react';
 import logo from './logo.svg';
 import { GlobalStyle } from './styles/GlobalStyles';
-import {AppContainer, AppHeader, AppLogo, AppTitle, AppIntro } from './components/UI'
+import {
+  AppContainer,
+  AppHeader,
+  AppLogo,
+  AppTitle,
+  AppIntro,
+} from './components/UI';
 import { useTheme } from './contexts/theme-context';
-
 
 function App() {
   const { theme } = useTheme();
   return (
-<>
-<GlobalStyle theme={theme} />
-     <AppContainer>
-       <AppHeader>
-         <AppLogo src={logo} alt="logo" />
-         <AppTitle>Welcome to React</AppTitle>
-       </AppHeader>
-       <AppIntro>
-         To get started, edit <code>src/App.js</code> and save to reload.
-       </AppIntro>
-     </AppContainer>
+    <>
+      <GlobalStyle theme={theme} />
+      <AppContainer>
+        <AppHeader>
+          <AppLogo src={logo} alt='logo' />
+          <AppTitle>Welcome to React</AppTitle>
+        </AppHeader>
+        <AppIntro>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </AppIntro>
+      </AppContainer>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider>
       <App />
-    </ThemeProvider>,
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
