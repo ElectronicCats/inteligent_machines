e29b507ec4e88cb3065049a80acdec64c74a6551
Alejandro Medina <5almero5@gmail.com>
2021-05-27 19:46:47 -0500

    Add canbas component

---
+++ src/components/Canvas/RelativeCanvas.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+import { Canvas } from './index';
+
+export const RelativeCanvas = styled(Canvas)`
+  position: absolute;
+  top: 0;
+  width: 93%;
+  height: 93%;
+`;
