446f227216286392403eec2857497f2b9e08c998
Alejandro Medina <5almero5@gmail.com>
2021-06-25 21:25:27 -0500

    Update card list component

---
+++ src/components/CourseList/utils.js
@@ -0,0 +1,51 @@
+import intro_ia from '../../assets/courses/intro_ia.svg';
+import etc_resp from '../../assets/courses/etc_resp.svg';
+import tyni_mach from '../../assets/courses/tyni_mach.svg';
+import our_kit from '../../assets/courses/our_kit.svg';
+import platform_edge from '../../assets/courses/platform_edge.svg';
+import dev_code from '../../assets/courses/dev_code.svg';
+import temp_sens from '../../assets/courses/temp_sens.svg';
+import color_sens from '../../assets/courses/color_sens.svg';
+import imu_sens from '../../assets/courses/imu_sens.svg';
+import micro_phon from '../../assets/courses/micro_phon.svg';
+import ia_vis from '../../assets/courses/ia_vis.svg';
+import curso_final from '../../assets/courses/curso_final.svg';
+
+export const CURSES_CARDS = [
+  { id: 'intro_ia', couseName: 'Introducción a IA', imagePath: intro_ia },
+  {
+    id: 'etc_resp',
+    couseName: `Ética &\nResponsabilidades`,
+    imagePath: etc_resp,
+  },
+  {
+    id: 'tyni_mach',
+    couseName: `Tiny Machine \nLearning`,
+    imagePath: tyni_mach,
+  },
+  { id: 'our_kit', couseName: 'Nuestro Kit', imagePath: our_kit },
+  {
+    id: 'platform_edge',
+    couseName: `Plataforma \n-Edge Impulse-`,
+    imagePath: platform_edge,
+  },
+  {
+    id: 'dev_code',
+    couseName: `Programando \nNuestro Código`,
+    imagePath: dev_code,
+  },
+  {
+    id: 'temp_sens',
+    couseName: `Sensor de \nTemperatura`,
+    imagePath: temp_sens,
+  },
+  {
+    id: 'color_sens',
+    couseName: `Sensor de \nColometría`,
+    imagePath: color_sens,
+  },
+  { id: 'imu_sens', couseName: `Sensor IMU \n9 ejes`, imagePath: imu_sens },
+  { id: 'micro_phon', couseName: `Micrófono`, imagePath: micro_phon },
+  { id: 'ia_vis', couseName: `Vision Artificial`, imagePath: ia_vis },
+  { id: 'curso_final', couseName: `Proyecto Final`, imagePath: curso_final },
+];
