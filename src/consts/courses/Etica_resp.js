2f51a4339d96fc1c2cb7b78b4e3ab1ac52914af5
Alejandro Medina <5almero5@gmail.com>
2021-07-07 19:30:46 -0500

    Add Etica data

---
+++ src/consts/courses/Etica_resp.js
@@ -0,0 +1,151 @@
+export const Etica_Resp = {
+  name: 'Ética y responsabilidades',
+  introContent: {
+    vimeoSrc: 'https://player.vimeo.com/video/522444610',
+    introClass: [
+      {
+        title: 'Objetivo',
+        description:
+          'Analizar los problemas que enfrenta la tecnología de inteligencia artificial actualmente y conocer las organizaciones que regulan el uso de esta tecnología. Adoptar un enfoque en materia de IA centrado en el ser humano, que tenga como objetivo reorientar el debate para incluir la función de la IA en la lucha contra las desigualdades actuales en materia de acceso al saber, a la investigación y a la diversidad de las expresiones culturales, y para que la IA no acentúe las diferencias tecnológicas entre los países y dentro de estos. La promesa de “la IA para todos” debe permitir que cada persona pueda sacar provecho de la revolución tecnológica en curso y acceder a sus beneficios, fundamentalmente en materia de innovación y saber.',
+        bullets: [],
+      },
+      {
+        title: 'Aprendizaje esperado',
+        description:
+          'Conocerá las responsabilidades que tienen los desarrolladores de algoritmos de IA y creadores de tecnología que apliquen estos algoritmos para resolver problemas aplicados a la vida del ser humano. ',
+      },
+      {
+        title: 'Competencia que favorece',
+        description:
+          'Comprende los retos a los que se enfrentan los desarrolladores de esta tecnología y la responsabilidad que tienen con la sociedad. Analiza los riesgos de una tecnología aplicada a las diferentes actividades y áreas de la vida del ser humano.',
+        bullets: [],
+      },
+      {
+        title: 'Contenidos temáticos',
+        description:
+          'Ética y responsabilidades de la IA, Organizaciones que regulan esta tecnología y riesgos de aplicar IA con datos sesgados.',
+      },
+    ],
+  },
+  planContent: [
+    {
+      theme: 'Limites Eticos de IA',
+      objective: 'Conocer los límites éticos de la IA con la vida humana.',
+      activitie: 'Expositiva',
+      material: 'Proyector, computadora y señalador',
+      classTime: '15 min',
+    },
+    {
+      theme: 'Responsabilidades de la IA',
+      objective:
+        'Analizar y comprender las responsabilidades que tienen los desarrolladores de esta tecnología',
+      activitie: 'Expositiva',
+      material: 'Proyector, computadora y señalador',
+      classTime: '15 min',
+    },
+    {
+      theme: 'Los 4 puntos a discusión de la IA',
+      objective:
+        'Debatir los 4 puntos que se encuentran en punto crítico sobre la IA.',
+      activitie:
+        'Forma 4 equipos en la clase, cada equipo tendrá un punto crítico de la IA, cada equipo propondrá alguna solución.',
+      material: 'Proyector, computadora y señalador',
+      classTime: '30 min',
+    },
+
+    {
+      theme: 'Organizaciones que regulan la IA en el mundo',
+      objective:
+        'Conocer las organizaciones que tienen como responsabilidad mantener la tecnología dentro de los límites éticos para obtener una mejor calidad de vida para todos.',
+      activitie:
+        'Buscar los objetivos de la agenda 2030 y  realizar una lluvia de ideas sobre cómo la IA puede mejorar los objetivos de esta tomando en cuenta la ética y responsabilidades. Leer la carta de la UNESCO redactada en la reunión de la ONU.',
+      material: 'Proyector, computadora, señalador, cartulina, marcadores',
+      classTime: '50 min',
+    },
+    {
+      theme: 'IA para todos',
+      objective: 'Generar conciencia sobre el uso de IA en nuestra vida.',
+      activitie:
+        'Generar una conclusión sobre cómo afectará nuestra IA en la vida del ser humano y que aspectos debe de cuidar en todo momento.',
+      material: 'Proyector, computadora, señalador',
+      classTime: '30 min',
+    },
+
+    {
+      theme: 'Cierre de módulo 2',
+      objective:
+        'Analizar si se tiene que tener en consideración algún punto crítico de la IA en el proyecto final.',
+      activitie:
+        'Descargar el material de la clase, contestar la práctica del módulo y resolver dudas por parte del instructor',
+      material: 'Evaluación de repaso',
+      classTime: '30 min',
+    },
+  ],
+  contentClass: {
+    downloadFileName: 'Contenido_Módulo_2',
+    downloadLabel: 'Descarga el material para impartir esta clase',
+    pathToDownloadFile: '',
+    content: [
+      {
+        title: 'Límites éticos y responsabilidades de la IA',
+        description:
+          'En esta clase conocerás cuales son los límites éticos y las responsabilidades que tiene la tecnología de Inteligencia Artificial con la responsabilidad',
+      },
+      {
+        title: 'Los 4 puntos a discusión de la IA',
+        description:
+          'En esta clase aprenderás cuales son los 4 puntos críticos de la inteligencia artificial, que están a discusión por organizaciones que están regulando esta tecnología.',
+      },
+      {
+        title: 'Organizaciones que regulan la IA en el mundo',
+        description:
+          'En esta clase aprenderás cuales son las organizaciones encargadas de regular la tecnología de Inteligencia Artificial',
+      },
+      {
+        title: 'IA para todos',
+        description:
+          'En esta clase conocerás porque la inteligencia artificial se encontrará en cada aspecto de nuestra vida en los próximos años.',
+      },
+    ],
+  },
+  practiceClass: {
+    precticeName: '',
+    time: '',
+    meterials: [''],
+    objective: '',
+    steps: '',
+  },
+  resoursesClass: {
+    mainLink: {
+      pathToFile:
+        'https://es.unesco.org/themes/tic-educacion/inteligencia-artificial',
+      downloadName: '',
+      label: 'Link a lectura',
+    },
+    links: [
+      {
+        pathToFile: 'https://www.moralmachine.net/hl/es',
+        downloadName: '',
+        label: 'Link a lectura',
+      },
+      {
+        pathToFile:
+          'https://tec.mx/es/noticias/guadalajara/investigacion/inteligencia-artificial-y-etica-tec-y-jalisco-las-impulsan-en',
+        downloadName: '',
+        label: 'Link a lectura',
+      },
+      {
+        pathToFile:
+          'https://www.thinkingheads.com/latam/tendencia-global/robotica-inteligencia-artificial-etica-debate/',
+        downloadName: '',
+        label: 'Link a lectura',
+      },
+      {
+        pathToFile:
+          'https://opendialogueonai.com/wp-content/uploads/2020/07/ES_Delib.pdf',
+        downloadName: '',
+        label: 'Link a lectura',
+      },
+    ],
+  },
+};
--- src/consts/courses/index.js
+++ src/consts/courses/index.js
@@ -1,6 +1,8 @@
 import { InteligenciaArtificial } from './Ai';
+import { Etica_Resp } from './Etica_resp';
 
 // eslint-disable-next-line
 export default {
   intro_ia: InteligenciaArtificial,
+  etc_resp: Etica_Resp,
 };
