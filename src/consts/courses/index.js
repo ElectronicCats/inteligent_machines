d91fb4762c41af331537cf9873e4e845bb92e3a3
Alejandro Medina <5almero5@gmail.com>
2021-07-07 19:52:03 -0500

    Add edge data

--- src/consts/courses/index.js
+++ src/consts/courses/index.js
@@ -2,11 +2,12 @@ import { InteligenciaArtificial } from './Ai';
 import { Etica_Resp } from './Etica_resp';
 import { Machine_learning } from './Ml';
 import { NuestroKit } from './Kit';
-
+import { EdgeImpulse } from './EdgeImp';
 // eslint-disable-next-line
 export default {
   intro_ia: InteligenciaArtificial,
   etc_resp: Etica_Resp,
   tyni_mach: Machine_learning,
   our_kit: NuestroKit,
+  platform_edge: EdgeImpulse,
 };
