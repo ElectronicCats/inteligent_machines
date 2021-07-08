d9096cf9705bbcda74f12add66953fedde77149d
Alejandro Medina <5almero5@gmail.com>
2021-07-07 19:58:54 -0500

    Add dev code data

--- src/consts/courses/index.js
+++ src/consts/courses/index.js
@@ -3,6 +3,7 @@ import { Etica_Resp } from './Etica_resp';
 import { Machine_learning } from './Ml';
 import { NuestroKit } from './Kit';
 import { EdgeImpulse } from './EdgeImp';
+import { DevCode } from './DevCode';
 // eslint-disable-next-line
 export default {
   intro_ia: InteligenciaArtificial,
@@ -10,4 +11,5 @@ export default {
   tyni_mach: Machine_learning,
   our_kit: NuestroKit,
   platform_edge: EdgeImpulse,
+  dev_code: DevCode,
 };
