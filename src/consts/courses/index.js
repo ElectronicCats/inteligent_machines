c704a680f75e737ce149c1ad02c5aaca3065c7d2
Alejandro Medina <5almero5@gmail.com>
2021-07-07 19:39:37 -0500

    Add machine Larnig data

--- src/consts/courses/index.js
+++ src/consts/courses/index.js
@@ -1,8 +1,10 @@
 import { InteligenciaArtificial } from './Ai';
 import { Etica_Resp } from './Etica_resp';
+import { Machine_learning } from './Ml';
 
 // eslint-disable-next-line
 export default {
   intro_ia: InteligenciaArtificial,
   etc_resp: Etica_Resp,
+  tyni_mach: Machine_learning,
 };
