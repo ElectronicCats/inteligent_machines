52da911191f1108feba2b73c7c8c9edc97efadff
Alejandro Medina <5almero5@gmail.com>
2021-07-07 19:46:29 -0500

    Add Kit Data

--- src/consts/courses/index.js
+++ src/consts/courses/index.js
@@ -1,10 +1,12 @@
 import { InteligenciaArtificial } from './Ai';
 import { Etica_Resp } from './Etica_resp';
 import { Machine_learning } from './Ml';
+import { NuestroKit } from './Kit';
 
 // eslint-disable-next-line
 export default {
   intro_ia: InteligenciaArtificial,
   etc_resp: Etica_Resp,
   tyni_mach: Machine_learning,
+  our_kit: NuestroKit,
 };
