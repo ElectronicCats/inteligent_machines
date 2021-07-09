bf12344fdf3f1c9e7ff230dd97b07a286efe68c7
Alejandro Medina <5almero5@gmail.com>
2021-07-08 19:51:48 -0500

    update Final Project

--- src/consts/courses/FinalProject.js
+++ src/consts/courses/FinalProject.js
@@ -6,31 +6,52 @@ export const FinalProject = {
     introClass: [
       {
         title: 'Objetivo',
-        description: '',
+        description:
+          'Desarrollar un proyecto utilizando los conocimientos adquiridos en este programa educativo',
         bullets: [],
       },
       {
         title: 'Aprendizaje esperado',
-        description: '',
+        description:
+          'Entenderá la estructura básica para documentar proyectos y aplicará los conocimientos de Machine Learning.',
       },
       {
         title: 'Competencia que favorece',
-        description: '',
+        description: 'Desarrollo de proyectos con Tiny Machine Learning',
         bullets: [],
       },
       {
         title: 'Contenidos temáticos',
-        description: '',
+        description:
+          'Arduino IDE, Microcontroladores, Sensores, Comunicación I2C, Edge Impulse, Análisis de datos, procesamiento de imágenes.',
       },
     ],
   },
   planContent: [
     {
-      theme: 'Tema1',
-      objective: '',
-      activitie: '',
-      material: '',
-      classTime: '30 min',
+      theme: 'Documentación del proyecto',
+      objective: 'Aprender a documentar cada etapa del proyecto ',
+      activitie:
+        'El instructor enseñará la estructura básica para documentar un proyecto de tecnología y realizarán el llenado de la información dependiendo del proyecto que cada quien escogió.',
+      material: 'Computadora, hojas y bolígrafo',
+      classTime: ' 480 min',
+    },
+    {
+      theme: 'Desarrollo del proyecto',
+      objective:
+        'Desarrollar el prototipo del proyecto, realizar pruebas correspondientes y analizar los resultados finales',
+      activitie:
+        'El instructor orientará al grupo para que tengan éxito en el desarrollo de su proyecto final.',
+      material: 'Depende de las necesidades del alumno y el proyecto',
+      classTime: '600 min',
+    },
+    {
+      theme: 'Presentación de proyecto',
+      objective:
+        'Presentar al grupo el resultado final del proyecto, prototipo, aplicación y análisis de resultados.',
+      activitie: 'Cada alumno o equipo presentará su proyecto en clase',
+      material: 'Prototipo, proyector y computadora',
+      classTime: '50 minutos por presentación',
     },
   ],
   contentClass: {
@@ -39,16 +60,9 @@ export const FinalProject = {
     pathToDownloadFile: '',
     content: [
       {
-        title: 'Introducción',
-        description: '',
-      },
-      {
-        title: 'Mi',
-        description: '',
-      },
-      {
-        title: 'Exportando',
-        description: '',
+        title: 'Mi proyecto final',
+        description:
+          'En esta clase se explicará en qué consiste el desarrollo de tu proyecto final, se explicará cada una de las partes que debe de contener la estructura del documento del proyecto.',
       },
     ],
   },
@@ -61,15 +75,15 @@ export const FinalProject = {
   },
   resoursesClass: {
     mainLink: {
-      pathToFile: 'https://teachablemachine.withgoogle.com/',
+      pathToFile: '',
       downloadName: '',
-      label: 'Página Teachable Machine',
+      label: '',
     },
     links: [
       {
         pathToFile: '',
         downloadName: '',
-        label: 'Video aprendiendo Teachable Machine',
+        label: '',
       },
     ],
   },
